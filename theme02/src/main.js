7d5dbd1368130b8784e39a5a935505220b47871b
Romain PIOT <romainpiot.pro@gmail.com>
2020-10-21 15:30:29 +0200

    update init project with vue2 & default components & router

--- theme02/src/main.js
+++ theme02/src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
 
-createApp(App).mount('#app')
+Vue.use(Router)
+
+
+const router = new Router({
+    routes: [
+    //   {
+    //     path: '/the-path/',
+    //     component: TheComponent
+    //   },
+      
+    ]
+  })
+  
+Vue.config.productionTip = false
+
+new Vue({
+    router, 
+    render: h => h(App),
+  }).$mount('#app')
+  
